20345e3bfa9b6e98711a7d458dd739df35ca3c96
Keegan <42782601+moon4433@users.noreply.github.com>
2023-05-01 00:25:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', (event) => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
