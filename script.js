102b4e983fdc21724ff4e3b530ba3165d73e945e
moon4433 <johnk117@ferris.edu>
2023-05-01 01:45:42 -0400

    Implementing Highscore

--- script.js
+++ script.js
@@ -14,9 +14,9 @@
 
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.floor(Math.random() * (20 - 1 + 1)) + 1);
+let secretNumber = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', (event) => {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +28,8 @@ document.querySelector('.check').addEventListener('click', (event) => {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    checkHighscore();
     // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -49,7 +51,31 @@ document.querySelector('.check').addEventListener('click', (event) => {
   }
 });
 
+document.querySelector('.again').addEventListener('click', (event) => {
+  reset();
+});
+
 function decreaseScore() {
   score--;
   document.querySelector('.score').textContent = score;
+}
+
+function checkHighscore() {
+  if (highscore < score) {
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+  }
+}
+
+function reset() {
+  score = 20;
+  secretNumber = Math.trunc(Math.floor(Math.random() * (20 - 1 + 1)) + 1);
+
+  document.querySelector('.message').textContent = '❓ Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 }
