f965958ee6b5ecb5eb3f250f82593b554c58b182
moon4433 <johnk117@ferris.edu>
2023-05-01 01:11:49 -0400

    Manipulating CSS Styles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // console.log(document.querySelector('.message').textContent);
 
@@ -16,34 +16,40 @@
 
 const secretNumber = Math.trunc(Math.floor(Math.random() * (20 - 1 + 1)) + 1);
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector(".check").addEventListener("click", (event) => {
-  const guess = Number(document.querySelector(".guess").value);
+document.querySelector('.check').addEventListener('click', (event) => {
+  const guess = Number(document.querySelector('.guess').value);
+  // when there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No number!";
+    document.querySelector('.message').textContent = '⛔ No number!';
+    // when player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too High!";
+      document.querySelector('.message').textContent = '📈 Too High!';
       decreaseScore();
     } else {
       decreaseScore();
-      document.querySelector(".message").textContent = "💥 You Lose!";
+      document.querySelector('.message').textContent = '💥 You Lose!';
     }
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too Low!";
+      document.querySelector('.message').textContent = '📉 Too Low!';
       decreaseScore();
     } else {
       decreaseScore();
-      document.querySelector(".message").textContent = "💥 You Lose!";
+      document.querySelector('.message').textContent = '💥 You Lose!';
     }
   }
 });
 
 function decreaseScore() {
   score--;
-  document.querySelector(".score").textContent = score;
+  document.querySelector('.score').textContent = score;
 }
